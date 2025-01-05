af7bdc6d91e777606dea01e41d9e086da42866c9
sahayamasha97 <sahayamasha97@gmail.com>
2025-01-05 10:58:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,296 @@
+var alphabet=(prompt("Enther the Alphabet Letter"));
+
+switch(alphabet)
+{
+    case "A":
+    case "a":    
+        
+    if(alphabet=="A")
+    {
+    document.write("APPLE");
+    }
+    else
+    {
+        document.write("apple");
+    }
+    break;
+    case "B":
+    case "b":
+        
+    if(alphabet=="B")
+    {
+    document.write("BUTTERFLY");
+    }
+    else
+    {
+        document.write("butterfly");
+    }
+    break;
+    case "C":
+    case "c":
+            
+    if(alphabet=="C")
+    {
+    document.write("CARROT");
+    }
+    else
+    {
+    document.write("carrot");
+    }
+    break;
+    case "D":
+    case "d":
+    if(alphabet=="D")
+    {
+    document.write("DIAMOND");
+    }
+    else
+    {
+    document.write("diamond");
+    }
+    break;
+    case "E":
+    case "e":
+    if(alphabet=="E")
+    {
+    document.write("EARTH");
+    }
+    else
+    {
+    document.write("earth");
+    }
+    break;
+    case "F":
+    case "f":
+    if(alphabet=="F")
+    {
+    document.write("FIRE");
+    }
+    else
+    {
+    document.write("fire");
+    }
+    break;    
+    case "G":
+    case "g":
+    if(alphabet=="G")
+    {
+    document.write("GIRAFFE");
+    }
+    else
+    {
+    document.write("giraffe");
+    }
+    break;             
+    case "H":
+    case "h":
+    if(alphabet=="H")
+    {
+    document.write("HONEY");
+    }
+    else
+    {
+    document.write("honey");
+    }
+    break;   
+    case "I":
+    case "i":
+    if(alphabet=="I")
+    {
+    document.write("ICE CREAM");
+    }
+    else
+    {
+    document.write("ice cream");
+    }
+    break;
+    case "J":
+    case "j":
+    if(alphabet=="J")
+    {
+    document.write("JUICE");
+    }
+    else
+    {
+    document.write("juice");
+    }
+    break;
+    case "K":
+    case "k":
+    if(alphabet=="K")
+    {
+    document.write("KEY");
+    }
+    else
+    {
+    document.write("key");
+    }
+    break;
+    case "L":
+    case "l":
+    if(alphabet=="L")
+    {
+    document.write("LION");
+    }
+    else
+    {
+    document.write("lion");
+    }
+    break;
+    case "M":
+    case "m":
+    if(alphabet=="M")
+    {
+    document.write("MOON");
+    }
+    else
+    {
+    document.write("moon");
+    }
+    break;
+    case "N":
+    case "n":
+    if(alphabet=="N")
+    {
+    document.write("NOTES");
+    }
+    else
+    {
+    document.write("notes");
+    }
+    break;
+    case "O":
+    case "o":
+    if(alphabet=="O")
+    {
+    document.write("OIL");
+    }
+    else
+    {
+    document.write("oil");
+    }
+    break;
+    case "p":
+    case "P":
+    if(alphabet=="P")
+    {
+    document.write("PANDA");
+    }
+    else
+    {
+    document.write("panda");
+    }
+    break;
+    case "Q":
+    case "q":
+    if(alphabet=="Q")
+    {
+    document.write("QUEEN");
+    }
+    else
+    {
+    document.write("queen");
+    }
+    break;
+    case "R":
+    case "r":
+    if(alphabet=="R")
+    {
+    document.write("ROSE");
+    }
+    else
+    {
+    document.write("rose");
+    }
+    break;
+    case "S":
+    case "s":
+    if(alphabet=="S")
+    {
+    document.write("STAR");
+    }
+    else
+    {
+    document.write("star");
+    }
+    break;
+    case "T":
+    case "t":
+    if(alphabet=="T")
+    {
+    document.write("TAXI");
+    }
+    else
+    {
+    document.write("taxi");
+    }
+    break;
+    case "U":
+    case "u":
+    if(alphabet=="U")
+    {
+    document.write("UNICORN");
+    }
+    else
+    {
+    document.write("unicorn");
+    }
+    break;
+    case "V":
+    case "v":
+    if(alphabet=="V")
+    {
+    document.write("VIOLINE");
+    }
+    else
+    {
+    document.write("violine");
+    }
+    break;
+    case "W":
+    case "w":
+    if(alphabet=="W")
+    {
+    document.write("WATERMELON");
+    }
+    else
+    {
+    document.write("watermelon");
+    }
+    break;
+    case "X":
+    case "x":
+    if(alphabet=="X")
+    {
+    document.write("X-MAS");
+    }
+    else
+    {
+    document.write("x-mas");
+    }
+    break;
+    case "Y":
+    case "y":
+    if(alphabet=="Y")
+    {
+    document.write("YOGA");
+    }
+    else
+    {
+    document.write("yoga");
+    }
+    break;
+    case "Z":
+    case "z":
+    if(alphabet=="Z")
+    {
+    document.write("ZERO");
+    }
+    else
+    {
+    document.write("zero");
+    }
+    break;
+
+}
+
